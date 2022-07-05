629e2d8724e28975505ff0693b15485d94c284f4
Aman Singh <amansingh.afk@gmail.com>
2022-07-06 01:23:42 +0530

    UI added

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,45 +2,119 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
-
-    <style>
-
-    </style>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <!-- CSS Files -->
+  <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="../assets/css/now-ui-dashboard.css?v=1.5.0" rel="stylesheet" />
 </head>
 
-<body>
-
-
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-2">
-                <h1 class="bg-danger"> SMS</h1>
-                {{-- ------------------------------------------------------------------ --}}
-
-                <a href="{{route('room.index')}}">Rooms</a>
-                <br>
-                <a href="{{route('course.index')}}">Cources</a>
-                <br>
-                <a href="{{route('professor.index')}}">Professors</a>
-                <br>
-                <a href="{{route('period.index')}}">Periods</a>
-                <br>
-                <a href="{{route('class.index')}}">Classes</a>
-                <br>
-
-                {{-- ------------------------------------------------------------------ --}}
-            </div>
-            <div class="col-md-10">
-                @yield('content')
-            </div>
-        </div>
+<body class="">
+  <div class="wrapper ">
+    <div class="sidebar" data-color="red"><!--Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+    -->
+      <div class="logo">
+        
+        <a href="" class="simple-text logo-normal">
+          SMS 
+        </a>
+      </div>
+      <div class="sidebar-wrapper" id="sidebar-wrapper">
+        <ul class="nav">
+          <li>
+            <a href="">
+              <i class="now-ui-icons design_palette"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{route('room.index')}}">
+              <i class="now-ui-icons business_bank"></i>
+              <p>Rooms</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{route('course.index')}}">
+              <i class="now-ui-icons education_agenda-bookmark"></i>
+              <p>Courses</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{route('professor.index')}}">
+              <i class="now-ui-icons education_hat"></i>
+              <p>Professors</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{route('class.index')}}">
+              <i class="now-ui-icons travel_istanbul"></i>
+              <p>Classes</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{route('period.index')}}">
+              <i class="now-ui-icons loader_refresh"></i>
+              <p>Periods</p>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <i class="now-ui-icons users_single-02"></i>
+              <p>User Account</p>
+            </a>
+          </li>
+          <li class="active-pro">
+            <a href="">
+              <i class="now-ui-icons media-1_button-power"></i>
+              <p>Logout</p>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-<script src="../../bootstrap/js/bootstrap.bundle.min.js"></script>
+    <div class="main-panel" id="main-panel">
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
+        <div class="container-fluid">
+          <div class="navbar-wrapper">
+            <div class="navbar-toggle">
+              <button type="button" class="navbar-toggler">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+              </button>
+            </div>
+            <a class="navbar-brand bg-dark px-4" href="#pablo">Dashboard</a>
+          </div>
+           </nav>
+      <!-- End Navbar -->
+      <!--   Core JS Files   -->
+  <script src="../assets/js/core/jquery.min.js"></script>
+  <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <!-- Chart JS -->
+  <script src="../assets/js/plugins/chartjs.min.js"></script>
+  <!--  Notifications Plugin    -->
+  <script src="../assets/js/plugins/bootstrap-notify.js"></script>
+  <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="../assets/js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script><!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
+  <script src="../assets/demo/demo.js"></script>
+  <script>
+    $(document).ready(function() {
+      // Javascript method's body can be found in assets/js/demos.js
+      demo.initDashboardPageCharts();
+
+    });
+  </script>
 </body>
 
 </html>
